678951ce471ab541daf1a5e2be37b88362b4babb
alihassan <24819103+thisalihassan@users.noreply.github.com>
2020-06-18 23:35:16 +0500

    path resolve server

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var port = process.env.PORT || 8001;
 
 var server = require("http"),
-  url = require("url"),
   path = require("path"),
   express = require("express"),
   cors = require("cors");
@@ -26,37 +25,20 @@ app.use(cors(corsOptions));
 app.use(express.json({ extended: false }));
 app.get("/", (request, response) => {
   try {
-    // response.setHeader("Access-Control-Allow-Origin", true);
-    // response.setHeader(
-    //   "Access-Control-Allow-Methods",
-    //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-    // );
+    response.setHeader("Access-Control-Allow-Origin", true);
+    response.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
 
-    // // Request headers you wish to allow
-    // response.setHeader(
-    //   "Access-Control-Allow-Headers",
-    //   "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
-    // );
-    // // Set to true if you need the website to include cookies in the requests sent
-    // response.setHeader("Access-Control-Allow-Credentials", true);
-    var uri = url.parse(request.url).pathname,
-      filename = path.join(process.cwd(), uri);
-    var stats;
-
-    try {
-      stats = fs.lstatSync(filename);
-    } catch (e) {
-      console.log("error");
-    }
-
-    if (fs.statSync(filename).isDirectory()) {
-      filename += "/index.html";
-    }
-    fs.readFile(filename, "utf8", function (err, file) {
-      response.writeHead(200);
-      response.write(file, "utf8");
-      response.end();
-    });
+    // Request headers you wish to allow
+    response.setHeader(
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+    );
+    // Set to true if you need the website to include cookies in the requests sent
+    response.setHeader("Access-Control-Allow-Credentials", true);
+    res.sendFile(path.resolve(__dirname, "index.html"));
   } catch (e) {
     console.log("error");
   }
